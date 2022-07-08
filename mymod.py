3971d32d940c7855e45ac3b1af21eaf8055b4aa6
Sarah-Stodder <107576959+Sarah-Stodder@users.noreply.github.com>
2022-07-07 21:52:31 -0600

    first-commit

---
+++ mymod.py
@@ -0,0 +1,15 @@
+from math import pi
+
+def my_house():
+    area1 = w * l 
+    print(f"Area of your house is: {area1}")
+w = float(input('Enter the width of house: '))
+l = float(input('Enter the length of house: '))
+
+
+def circumference():
+    radius = float(input("Please choose an radius for your circle: ")) 
+    circ = 2 * pi * radius      
+    print(f"The circumference is: {circ}") 
+    return
+
